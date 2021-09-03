51b9576f1a0ea002f1bd849f65b98e88b2a25918
parimeo <daniel.gruca@parimeo.de>
2021-09-03 12:47:16 +0200

    add text-display component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TextDisplayComponent } from './text-display/text-display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextDisplayComponent
   ],
   imports: [
     BrowserModule,
